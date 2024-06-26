27943fcac735f4c05420392ccaa9f600f6482096
Rohyu-jeong <95173047+Rohyu-jeong@users.noreply.github.com>
2024-06-26 10:55:55 +0900

    Delete src/SNSButton directory

--- src/SNSButton/SNSNames.tsx
+++
@@ -1,9 +0,0 @@
-export type SNSNamesProps = {
-    name: "Google" | "Facebook" | "Apple";
-}
-
-const SNSNames = ({name}:SNSNamesProps) => {
-    return <span>Start with {name}</span>;
-}
-
-export default SNSNames;
